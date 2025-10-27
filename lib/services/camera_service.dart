b622f60bca81ec2433800b1de9196a82e4c618df
nendix <nendix12@gmail.com>
2025-10-27 20:38:39 +0100

    Clean up camera service imports

--- lib/services/camera_service.dart
+++ lib/services/camera_service.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 import 'package:permission_handler/permission_handler.dart';
